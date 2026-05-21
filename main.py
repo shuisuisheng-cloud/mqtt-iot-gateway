74ab7aa62c73258f3ffb3c840abc44ed2d35dbfc
shuisuisheng-cloud <shuisuisheng@gmail.com>
2026-05-21 20:07:50 +0800

    add exception handing for invalid serial data

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ def check_temperature(temp):
     else:
         return "normal"
 def read_serial_data():
-    return "temperature:"+str(round(random.uniform(20,35),1))
+    return "temperature:"+str(round(random.uniform(20,40),1))
 def parse_temperature(data):
-    return float(data.split(":")[1])
+    try:
+        part=data.split(":")
+        temperature=float(part[1])
+        return temperature
+    except:
+        return None
 def main():
     project_name="linux-serial-tool"
     verision="v0.1"
@@ -23,11 +28,16 @@ def main():
     while count <=5:
         serial_data=read_serial_data()
         temperature=parse_temperature(serial_data)
-        temperature_str=str(temperature)
-        time.sleep(1)
-        log="temperature:"+temperature_str+" "+"status:"+check_temperature(temperature)
-        save_line(str(log))
-        print(log)
+        if temperature is None:
+            save_line("invalid data:"+serial_data)
+            print("invalid data")
+        else:
+            temperature_str=str(temperature)
+            time.sleep(1)
+            log="temperature:"+temperature_str+" "+"status:"+check_temperature(temperature)
+            save_line(str(log))
+            print(log)
         count +=1
 if __name__ == "__main__":    
     main()
+# "temperature:"+str(round(random.uniform(20,40),1))
