4615e40ec2ac3d652a1351144ce67fbe1a875968
shuisuisheng-cloud <shuisuisheng@gmail.com>
2026-06-02 14:59:20 +0800

    add timestamp to projects

--- main.py
+++ main.py
@@ -19,20 +19,21 @@ def parse_temperature(data):
         return temperature
     except:
         return None
-def build_device_data(device,temperature,status):
-    return {"device":device,"temperature":temperature,"status":status}
+def build_device_data(device,temperature,status,time):
+    return {"device":device,"temperature":temperature,"status":status,"timestamp":time}
 def handle_json_data(temperature,device):
     status=check_temperature(temperature)
-    device_data=build_device_data(device,temperature,status)
+    timestatus=time.strftime("%Y-%m-%d %H:%M:%S")
+    device_data=build_device_data(device,temperature,status,timestatus)
     json_data=json.dumps(device_data)
     save_line(json_data)
     time.sleep(1)
-    print("valid data:","temperature:",device_data["temperature"],"status:",device_data["status"])
-def handle_invalid_data(device,data):
-    line="device:"+device+"invalid_data:"+data
+    print("valid data:","temperature:",device_data["temperature"],"status:",device_data["status"],"timestamp:",device_data["timestamp"])
+def handle_invalid_data(device,data,timestatus):
+    line="device:"+" "+device+" "+"invalid_data:"+" "+data+" "+"timestamp:"+" "+timestatus
     save_line(line)
     time.sleep(1)
-    print("invalid data:"+data)
+    print("invalid data:" + data,"timestamp:" + timestatus)
 def main():
     project_name="linux-serial-tool"
     verision="v0.1"
@@ -41,10 +42,11 @@ def main():
     baudrate=9600
     device = "stm32_node_01"
     test_data = ["temperature:28.6","temperature:abc","error_data","temperature:","temperature:31.5"]
+    timestatus=time.strftime("%Y-%m-%d %H:%M:%S")
     for serial_data in test_data:
         temperature=parse_temperature(serial_data)
         if temperature is None:
-            handle_invalid_data(device,serial_data)
+            handle_invalid_data(device,serial_data,timestatus)
         else:
             handle_json_data(temperature,device)
 if __name__ == "__main__":    
